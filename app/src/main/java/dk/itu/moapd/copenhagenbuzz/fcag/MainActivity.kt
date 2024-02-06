6daa618f03606ed606b961268793ea713162ab2d
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-06 15:42:21 +0100

    Added the dropdown input for event type

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainActivity.kt
@@ -1,19 +1,37 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.AutoCompleteTextView
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mybutton: Button
-    private lateinit var displayText : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        createDropdownEventType()
+    }
+
+
+    /**
+     *
+     * DropDown Item for the Event Type.
+     *
+     * val eventTypes is the string array of options in res/values/strings.xml "event_types"
+     * val arrayAdapter is
+     * val autoCompleteTextView is
+     *
+     */
+    private fun createDropdownEventType() {
+        val eventTypes = resources.getStringArray(R.array.event_types)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item_layout, eventTypes)
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        autoCompleteTextView.setAdapter(arrayAdapter)
     }
 
 

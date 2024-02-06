6c1cd34ba6b98803a3f518241f88fda6e9ef227d
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-06 14:26:49 +0100

    First commit, added files

---
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainActivity.kt
@@ -0,0 +1,26 @@
+package dk.itu.moapd.copenhagenbuzz.fcag
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mybutton: Button
+    private lateinit var displayText : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+
+
+
+
+}

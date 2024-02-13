cf5b6f91d5d195c09294d38828a5f6999aae80f8
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-13 20:27:21 +0100

    Added intent, Go to MainActivity through loginButton in LoginActivity

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/LoginActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/LoginActivity.kt
@@ -1,11 +1,36 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import dk.itu.moapd.copenhagenbuzz.fcag.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
+    // GUI Variables
+    private lateinit var loginButton: Button
+    private lateinit var userButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Variables
+        loginButton = binding.loginButton
+        userButton = binding.userButton
+
+
+        with(binding) {
+            loginButton.setOnClickListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }

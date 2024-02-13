9342b09e0e4c318a335cb283f737063d56d88af4
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-13 10:55:43 +0100

    Modifie the Event.kt Data Class

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/Event.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/Event.kt
@@ -1,3 +1,8 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
-data class Event()
+data class Event(val eventName: String,
+                 var eventLocation: String,
+                 val startDate: Long,
+                 val endDate: Long,
+                 val eventType: String,
+                 var eventDescription: String)

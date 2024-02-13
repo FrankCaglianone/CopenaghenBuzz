7e5d1bd70a2fc297569fe949161c1df1e1bbf678
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-13 14:40:12 +0100

    Binding the MainActivity.kt

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/Event.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/Event.kt
@@ -1,8 +1,10 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
+import android.widget.Button
+
 data class Event(val eventName: String,
                  var eventLocation: String,
-                 val startDate: Long,
-                 val endDate: Long,
+                 val eventDate: String,
                  val eventType: String,
-                 var eventDescription: String)
+                 var eventDescription: String,
+                 var addEventButton: Button)
--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainActivity.kt
@@ -1,20 +1,56 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
+import dk.itu.moapd.copenhagenbuzz.fcag.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+
+    // GUI Variables
+    private lateinit var eventName : EditText
+    private lateinit var eventLocation : EditText
+    private lateinit var eventDate : EditText
+    private lateinit var eventType : AutoCompleteTextView
+    private lateinit var eventDescription : EditText
+    private lateinit var addEventButton: Button
+
+
+    // A set of private constants used in this class .
+    companion object {
+        private val TAG = MainActivity::class.qualifiedName
+    }
+
+
+// An instance of the ‘Event' class.
+//    private val event : Event = Event("", "")
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Variables
+        eventName = binding.editTextEventName
+        eventLocation = binding.editTextEventLocation
+        eventDate = binding.editTextEventDate
+        eventType = binding.autoCompleteTextViewEventType
+        eventDescription = binding.editTextEventDescription
+        addEventButton = binding.addEventButton
 
         createDropdownEventType()
+
+        okok()
     }
 
 
@@ -30,9 +66,35 @@ class MainActivity : AppCompatActivity() {
     private fun createDropdownEventType() {
         val eventTypes = resources.getStringArray(R.array.event_types)
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item_layout, eventTypes)
-        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.auto_complete_text_view_event_type)
         autoCompleteTextView.setAdapter(arrayAdapter)
     }
+
+
+
+    private fun okok() {
+//        addEventButton.setOnClickListener {
+//            if (eventLocation.text.toString().isNotEmpty() && eventName.text.toString().isNotEmpty()) {
+//                event.setEventName(
+//                    eventName.text.toString().trim()
+//                )
+
+                // Write in the ‘Logcat‘ system
+//                showMessage()
+//            }
+//        }
+    }
+
+
+
+
+//    private fun showMessage() {
+//        Log.d(TAG, event.toString())
+//    }
+
+
+
+
 
 
 

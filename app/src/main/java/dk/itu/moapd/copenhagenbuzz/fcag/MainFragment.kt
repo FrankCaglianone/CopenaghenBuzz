2b66ca12e9bfc1178d69d1f00b666e05574866cd
Francesco Caglianone <francescocaglianone@gmail.com>
2024-02-26 18:10:38 +0100

    Created fragment

---
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MainFragment.kt
@@ -0,0 +1,38 @@
+package dk.itu.moapd.copenhagenbuzz.fcag
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dk.itu.moapd.copenhagenbuzz.fcag.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding : FragmentMainBinding? = null
+
+    private val binding
+        get() = requireNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentMainBinding.inflate(inflater, container, false).also {
+        _binding = it
+    }.root
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

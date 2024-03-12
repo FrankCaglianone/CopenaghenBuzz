6b7b94d451fdb27450acd43e4900df2c0a3b8868
Francesco Caglianone <francescocaglianone@gmail.com>
2024-03-12 20:14:12 +0100

    Added other Sign in options

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/activities/LoginActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/activities/LoginActivity.kt
@@ -47,8 +47,8 @@ class LoginActivity : AppCompatActivity() {
         // Choose authentication providers.
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
-//            AuthUI.IdpConfig.PhoneBuilder().build(),
-//            AuthUI.IdpConfig.GoogleBuilder().build()
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
         )
         // Create and launch sign-in intent.
         val signInIntent = AuthUI.getInstance()

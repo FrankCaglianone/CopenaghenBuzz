2f5ad87808ba32a06129a301d7a60c3e097070e4
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-06 21:12:10 +0200

    Succesfully FETCHING DATA FROM FIREBASE!!!!!!!!!!!!!!

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/adapters/EventAdapter.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/adapters/EventAdapter.kt
@@ -5,14 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import com.firebase.ui.database.FirebaseListAdapter
+import com.firebase.ui.database.FirebaseListOptions
 import dk.itu.moapd.copenhagenbuzz.fcag.models.Event
 import dk.itu.moapd.copenhagenbuzz.fcag.R
 
-class EventAdapter(private val context: Context, private val events: List<Event>) : BaseAdapter() {
+class EventAdapter(options: FirebaseListOptions<Event>) : FirebaseListAdapter<Event>(options) {
 
     companion object {
         private var TAG = "EventAdapter.kt"
@@ -20,7 +21,61 @@ class EventAdapter(private val context: Context, private val events: List<Event>
 
 
 
-    class ViewHolder(val view: View) {
+//    class ViewHolder(val view: View) {
+//        val eventName: TextView = view.findViewById(R.id.event_name)
+//        val userImage: ImageView = view.findViewById(R.id.user_image)
+//        val favouriteButton: ImageButton = view.findViewById(R.id.favourite_button)
+//        val eventType: TextView = view.findViewById(R.id.event_type)
+//        val eventImage: ImageView = view.findViewById(R.id.event_image)
+//        val eventLocation: TextView = view.findViewById(R.id.event_location)
+//        val eventDate: TextView = view.findViewById(R.id.event_date)
+//        val eventDescription: TextView = view.findViewById(R.id.event_description)
+//    }
+
+
+//    override fun getCount(): Int { return events.size }
+//    override fun getItem(position: Int): Event { return events[position] }
+//    override fun getItemId(position: Int): Long { return position.toLong() }
+
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        // The old view should be reused, if possible.
+//        // If convertView == null -> inflate, else reuse the old view
+//        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.event_row_item, parent, false)
+//
+//        //
+//        val viewHolder = (view.tag as? ViewHolder) ?: ViewHolder(view)
+//
+//        // Populate the view holder with the selected `Dummy` data.
+//        Log.d(TAG, "Populate an item at position: $position")
+//        getItem(position).let { event ->
+//            populateViewHolder(viewHolder, event)
+//        }
+//
+//        // Set the new view holder and return the view object.
+//        view.tag = viewHolder
+//        return view
+//    }
+
+
+
+
+//    private fun populateViewHolder(viewHolder: ViewHolder, dummy: Event) {
+//        with(viewHolder) {
+//            eventName.text = dummy.eventName
+//            userImage.setImageResource(R.drawable.baseline_person)
+//            favouriteButton.setImageResource(R.drawable.baseline_favorite)
+//            eventType.text = dummy.eventType
+//            eventImage.setImageResource(R.drawable.ic_launcher_foreground)
+//            eventLocation.text = dummy.eventLocation
+//            eventDate.text = dummy.eventDate
+//            eventDescription.text = dummy.eventDescription
+//        }
+//
+//
+//    }
+
+    override fun populateView(view: View, dummy: Event, position: Int) {
+        // Find the views to populate in inflated template
         val eventName: TextView = view.findViewById(R.id.event_name)
         val userImage: ImageView = view.findViewById(R.id.user_image)
         val favouriteButton: ImageButton = view.findViewById(R.id.favourite_button)
@@ -29,48 +84,17 @@ class EventAdapter(private val context: Context, private val events: List<Event>
         val eventLocation: TextView = view.findViewById(R.id.event_location)
         val eventDate: TextView = view.findViewById(R.id.event_date)
         val eventDescription: TextView = view.findViewById(R.id.event_description)
-    }
 
 
-    override fun getCount(): Int { return events.size }
-    override fun getItem(position: Int): Event { return events[position] }
-    override fun getItemId(position: Int): Long { return position.toLong() }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        // The old view should be reused, if possible.
-        // If convertView == null -> inflate, else reuse the old view
-        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.event_row_item, parent, false)
-
-        //
-        val viewHolder = (view.tag as? ViewHolder) ?: ViewHolder(view)
-
-        // Populate the view holder with the selected `Dummy` data.
-        Log.d(TAG, "Populate an item at position: $position")
-        getItem(position).let { event ->
-            populateViewHolder(viewHolder, event)
-        }
-
-        // Set the new view holder and return the view object.
-        view.tag = viewHolder
-        return view
-    }
-
-
-
-
-    private fun populateViewHolder(viewHolder: ViewHolder, dummy: Event) {
-        with(viewHolder) {
-            eventName.text = dummy.eventName
-            userImage.setImageResource(R.drawable.baseline_person)
-            favouriteButton.setImageResource(R.drawable.baseline_favorite)
-            eventType.text = dummy.eventType
-            eventImage.setImageResource(R.drawable.ic_launcher_foreground)
-            eventLocation.text = dummy.eventLocation
-            eventDate.text = dummy.eventDate
-            eventDescription.text = dummy.eventDescription
-        }
-
-
+        // Populate the data into the template view using the data object
+        eventName.text = dummy.eventName
+        userImage.setImageResource(R.drawable.baseline_person)
+        favouriteButton.setImageResource(R.drawable.baseline_favorite)
+        eventType.text = dummy.eventType
+        eventImage.setImageResource(R.drawable.ic_launcher_foreground)
+        eventLocation.text = dummy.eventLocation
+        eventDate.text = dummy.eventDate
+        eventDescription.text = dummy.eventDescription
     }
 
 }
--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/TimelineFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/TimelineFragment.kt
@@ -6,9 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseListOptions
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dk.itu.moapd.copenhagenbuzz.fcag.R
 import dk.itu.moapd.copenhagenbuzz.fcag.models.DataViewModel
 import dk.itu.moapd.copenhagenbuzz.fcag.adapters.EventAdapter
+import dk.itu.moapd.copenhagenbuzz.fcag.adapters.FavoriteAdapter
 import dk.itu.moapd.copenhagenbuzz.fcag.databinding.FragmentTimelineBinding
+import dk.itu.moapd.copenhagenbuzz.fcag.models.Event
+import io.github.cdimascio.dotenv.dotenv
 
 
 class TimelineFragment : Fragment() {
@@ -22,31 +32,61 @@ class TimelineFragment : Fragment() {
         }
 
 
-
-    private val eventListViewModel: DataViewModel by viewModels()
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val dotenv = dotenv {
+        directory = "/assets"
+        filename = "env"
     }
+    private val DATABASE_URL = dotenv["DATABASE_URL"]
+
+
+
+//    private val eventListViewModel: DataViewModel by viewModels()
+
+
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = FragmentTimelineBinding.inflate(inflater, container, false).also {
         _binding = it
+
+
+        FirebaseAuth.getInstance().currentUser?.let { user ->
+            val query = Firebase.database(DATABASE_URL).reference
+                .child("events")
+                .child(user.uid)
+//                .orderByChild("createdAt")
+
+            val options = FirebaseListOptions.Builder<Event>()
+                .setLayout(R.layout.event_row_item)
+                .setQuery(query, Event::class.java)
+                .setLifecycleOwner(this)
+                .build()
+
+            // Initialize your FirebaseRecyclerAdapter with the options
+            val adapter = EventAdapter(options)
+
+            binding.listView.adapter = adapter
+
+            // Important: Start listening for database changes
+            adapter.startListening()
+        }
+
     }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.apply {
-            eventListViewModel.events.observe(viewLifecycleOwner) {tmp ->
-                // Define the list view adapter.
-                val adapter = EventAdapter(requireContext(), tmp)
-                listView.adapter = adapter
-            }
-        }
+//        binding.apply {
+//            eventListViewModel.events.observe(viewLifecycleOwner) {tmp ->
+//                // Define the list view adapter.
+//                val adapter = EventAdapter(requireContext(), tmp)
+//                listView.adapter = adapter
+//            }
+//        }
     }
 
 

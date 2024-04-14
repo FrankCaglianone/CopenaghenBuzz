81e71c169665820446a1ecbdc36ee149e70f3c7c
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-14 17:06:41 +0200

    Implemented the map

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MyApplication.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MyApplication.kt
@@ -1,14 +1,36 @@
 package dk.itu.moapd.copenhagenbuzz.fcag
 
 import android.app.Application
+import android.util.Log
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
 import com.google.android.material.color.DynamicColors
 
-class MyApplication : Application() {
+class MyApplication : Application(), OnMapsSdkInitializedCallback {
+
+    companion object {
+        var TAG = "MyApplication"
+    }
 
     override fun onCreate() {
         super.onCreate()
         // Apply dynamic colors to activities if available.
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        // Initialize google maps
+        MapsInitializer.initialize(this, MapsInitializer.Renderer.LATEST, this);
+    }
+
+
+    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
+        when (renderer) {
+            MapsInitializer.Renderer.LATEST -> {
+                Log.d(TAG, "The latest version of the renderer is used.")
+            }
+            MapsInitializer.Renderer.LEGACY -> {
+                Log.d(TAG, "The legacy version of the renderer is used.")
+            }
+        }
     }
 
 }
--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
@@ -1,12 +1,22 @@
 package dk.itu.moapd.copenhagenbuzz.fcag.fragments
 
+import android.content.pm.PackageManager
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import dk.itu.moapd.copenhagenbuzz.fcag.databinding.FragmentMapsBinding
 
+
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
 
@@ -17,6 +27,12 @@ class MapsFragment : Fragment() {
         get() = requireNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+
+
+    companion object {
+        private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +47,55 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.apply { }
+        val mapFragment = childFragmentManager
+            .findFragmentById(binding.map.id) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        // Add a marker in IT University of Copenhagen and move the camera.
+        val itu = LatLng(55.6596, 12.5910)
+        googleMap.addMarker(MarkerOptions().position(itu).title("IT University of Copenhagen"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(itu))
+
+        // Move the Google Maps UI buttons under the OS top bar.
+        googleMap.setPadding(0, 100, 0, 0)
+
+        // Enable the location layer. Request the permission if it is not granted.
+        if (checkPermission()) {
+            googleMap.isMyLocationEnabled = true
+        } else {
+            requestUserPermissions()
+        }
+    }
+
+
+
+    private fun checkPermission() =
+        ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+
+
+    private fun requestUserPermissions() {
+        if (!checkPermission())
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+            )
     }
 
 

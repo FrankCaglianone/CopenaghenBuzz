fbe0bcc6c7a0153d39a2291ede8aab10d08ea69e
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-16 18:19:05 +0200

    Code cleaning

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/CreateEventFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/CreateEventFragment.kt
@@ -38,12 +38,6 @@ class CreateEventFragment : Fragment() {
         }
 
 
-//    private val dotenv = dotenv {
-//        directory = "/assets"
-//        filename = "env"
-//    }
-//    private val apiKey = dotenv["GEOCODING_API_KEY"]
-
 
     // A set of private constants used in this class.
     companion object {
@@ -137,58 +131,8 @@ class CreateEventFragment : Fragment() {
     // validate the inputs provided
     private fun validateInputs(): Boolean {
         // Simple validation for example purposes
-        return binding.editTextEventLocation.text.toString().isNotEmpty() &&
-                binding.editTextEventName.text.toString().isNotEmpty()
+        return binding.editTextEventLocation.text.toString().isNotEmpty() && binding.editTextEventName.text.toString().isNotEmpty()
     }
-
-
-
-
-//    private suspend fun getLocationCoordinates(location: String): EventLocation {
-//        val baseUrl = "https://geocode.maps.co/search"
-//        val encodedLocation = URLEncoder.encode(location, "UTF-8")
-//        val urlString = "$baseUrl?q=$encodedLocation&api_key=$apiKey"
-//
-//        return withContext(Dispatchers.IO) {
-//            try {
-//                val url = URL(urlString)
-//                val connection = url.openConnection() as HttpURLConnection
-//                connection.requestMethod = "GET"
-//                connection.setRequestProperty("Content-type", "application/json")
-//                connection.setRequestProperty("Accept", "application/json")
-//                connection.connectTimeout = 10000
-//                connection.readTimeout = 10000
-//
-//                val reader = InputStreamReader(connection.inputStream)
-//                val response = StringBuilder()
-//                val bufferedReader = BufferedReader(reader)
-//
-//                bufferedReader.useLines { lines ->
-//                    lines.forEach { line ->
-//                        response.append(line.trim())
-//                    }
-//                }
-//
-//                val jsonArray = JSONArray(response.toString())
-//
-//                if (jsonArray.length() > 0) {
-//                    val firstLocation = jsonArray.getJSONObject(0)
-//                    val latitude = firstLocation.optString("lat")
-//                    val longitude = firstLocation.optString("lon")
-//                    Log.d(TAG, "IT WORKS lat:$latitude long:$longitude")
-//                    EventLocation(latitude.toDouble(), longitude.toDouble(), location)
-//                } else {
-//                    EventLocation(null, null, "No location found")
-//                }
-//            } catch (e: Exception) {
-//                EventLocation(null, null, "Error: ${e.message}")
-//            }
-//        }
-//    }
-
-
-
-
 
 
 }

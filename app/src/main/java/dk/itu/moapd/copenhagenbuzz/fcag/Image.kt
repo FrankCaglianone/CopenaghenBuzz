3eebfe86054a436bd2fe3521045d87b14e9e2cce
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-21 21:13:40 +0200

    Code cleaning

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/Image.kt
+++
@@ -1,6 +0,0 @@
-package dk.itu.moapd.copenhagenbuzz.fcag
-
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class Image(val url: String? = null, val path: String? = null, val createdAt: Long? = null)

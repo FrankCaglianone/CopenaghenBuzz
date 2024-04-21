fc230a90f19841d8e251551bda3538cc29a3a450
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-21 13:21:58 +0200

    Implemented marker fetching in the map, adding markers of created events

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
@@ -3,6 +3,7 @@ package dk.itu.moapd.copenhagenbuzz.fcag.fragments
 import android.content.pm.PackageManager
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,10 +14,19 @@ import dk.itu.moapd.copenhagenbuzz.fcag.databinding.FragmentMapsBinding
 
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dk.itu.moapd.copenhagenbuzz.fcag.data.Event
+import io.github.cdimascio.dotenv.dotenv
 
 class MapsFragment : Fragment() {
 
@@ -27,6 +37,14 @@ class MapsFragment : Fragment() {
         get() = requireNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+
+
+    // Enviroment Variables
+    private val dotenv = dotenv {
+        directory = "/assets"
+        filename = "env"
+    }
+    private val DATABASE_URL = dotenv["DATABASE_URL"]
 
 
     companion object {
@@ -77,6 +95,8 @@ class MapsFragment : Fragment() {
         } else {
             requestUserPermissions()
         }
+
+        addMarkers(googleMap)
     }
 
 
@@ -96,6 +116,50 @@ class MapsFragment : Fragment() {
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
             )
+    }
+
+
+
+
+
+
+
+    private fun addMarkers(map: GoogleMap) {
+        FirebaseAuth.getInstance().currentUser?.let { user ->
+            val userEventsRef = Firebase.database(DATABASE_URL).reference.child("copenhagen_buzz")
+                .child("events")
+                .child(user.uid)
+
+
+            userEventsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        snapshot.children.forEach { child ->
+                            val event = child.getValue(Event::class.java)
+                            event?.let {
+                                val location =
+                                    it.eventLocation?.latitude?.let { it1 -> it.eventLocation!!.longitude?.let { it2 ->
+                                        LatLng(it1,
+                                            it2
+                                        )
+                                    } }
+                                location?.let { it1 -> MarkerOptions().position(it1).title(it.eventName) }
+                                    ?.let { it2 ->
+                                        map.addMarker(
+                                            it2
+                                        )
+                                    }
+                            }
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("Firebase", "Error fetching data", error.toException())
+                }
+            })
+
+        }
     }
 
 

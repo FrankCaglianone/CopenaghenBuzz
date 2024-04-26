fbe95f9b313908594cce35763fd24dc708641737
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-26 20:08:42 +0200

    LocationCliente docstring

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/locationServices/LocationClient.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/locationServices/LocationClient.kt
@@ -5,8 +5,23 @@ import android.location.Location
 import kotlinx.coroutines.flow.Flow
 
 interface LocationClient {
+
+
+    /**
+     * Provides a Flow of Location objects, each representing the user's current location at
+     * specified intervals. This method allows for real-time location tracking.
+     *
+     * @param interval The desired interval between location updates in milliseconds.
+     * @return A Flow stream of Location objects, enabling the consumer to handle location data reactively.
+     * @throws LocationException If there are issues setting up the location updates.
+     */
     fun getLocationUpdates(interval: Long): Flow<Location>
 
 
+
+    /**
+     * Exception to be thrown when there are problems retrieving location updates.
+     * @param message Detailed message describing the error condition.
+     */
     class LocationException(message: String): Exception()
 }

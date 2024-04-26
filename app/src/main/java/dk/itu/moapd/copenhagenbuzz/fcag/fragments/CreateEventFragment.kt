3a3e89207863417c280d424d8f7b03667c26bfdd
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-26 19:02:22 +0200

    CreateEventFragment code cleaning

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/CreateEventFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/CreateEventFragment.kt
@@ -36,7 +36,7 @@ import java.io.ByteArrayOutputStream
 class CreateEventFragment : Fragment() {
 
 
-    // Enviroment Variables
+    // Environment Variables
     private val dotenv = dotenv {
         directory = "/assets"
         filename = "env"
@@ -64,32 +64,36 @@ class CreateEventFragment : Fragment() {
     }
 
 
-    // An instance of the ‘Event' class.
+    // An instance of the "Event" and "EventLocation" class.
     private val location: EventLocation = EventLocation(0.0, 0.0, "")
     private val event : Event = Event("", location, "", "", "")
 
 
 
-    // Declaring an instance of the CrudOperations class.
+    // Declaring variables used in this class
     private lateinit var crud: CrudOperations
     private lateinit var geocode: Geocoding
-
-
-    // Declaring variables for Firebase Storage
     private var imageUri: Uri? = null
     private lateinit var auth: FirebaseAuth
 
 
+
+
+
+    // Called when the fragment is being created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Creating an instance of the CrudOperations class.
         crud = CrudOperations()
+        // Creating an instance of the Geocoding class.
         geocode = Geocoding()
+        // Getting the instance from Firebase Authentication
         auth = FirebaseAuth.getInstance()
     }
 
 
 
+    // Called to create the layout of the fragment.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -101,14 +105,16 @@ class CreateEventFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Adding the Event Listener to Create a new Event
+        // Adding the click listener to create a new Event Object
         createEventListener()
+        // Adding the click listener to add an image to the Event Object
         addImageEventListener()
     }
 
 
 
 
+    // Called when the view previously created by onCreateView() has been detached from the fragment.
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -119,10 +125,17 @@ class CreateEventFragment : Fragment() {
 
 
     /**
-     * Creates an event listener that responds to clicks on the "Add Event" button by collecting
-     * input from the user and creating a new event instance. It validates the user input before
-     * proceeding to create an event and displays a message upon successful creation calling
-     * showMessage().
+     * This method adds on click listener to the "Add Event" button.
+     * It calls validateInputs() to verify that at least the name and the location of the event
+     * where inserted avoiding the creation of null events.
+     * It than fetches all the information added in a coroutine, needed for the geocoding services
+     * to calculate the latitude and longitude of the address of the event.
+     * If the user did not add an image for the event it displays a message asking to add an image
+     * insuring that the user adds an image for each event (mandatory ex. 09).
+     * If the user selected an image, the filename for that image gets created using
+     * System.currentTimeMillis().
+     * The event is than added to the Firebase Realtime Database and the image is added to the
+     * Firebase Storage.
      */
     private fun createEventListener() {
         with(binding) {
@@ -165,7 +178,11 @@ class CreateEventFragment : Fragment() {
 
 
 
-    // validate the inputs provided
+
+    /**
+     * Validate the inputs inserted in creating the event, to make sure is Name are location are
+     * inserted
+     */
     private fun validateInputs(): Boolean {
         // Simple validation for example purposes
         return binding.editTextEventLocation.text.toString().isNotEmpty() && binding.editTextEventName.text.toString().isNotEmpty()
@@ -173,22 +190,36 @@ class CreateEventFragment : Fragment() {
 
 
 
+
+
+
+    /**
+     * This method adds the click listener to the add Image button to add an image to the Event.
+     * It created an AlertDialog asking if the user would like to upload the photo from Google Photo
+     * or take a new photo from the camera.
+     */
     private fun addImageEventListener() {
        binding.addImageButton.setOnClickListener {
            AlertDialog.Builder(it.context)
                .setTitle("Add Image")
                .setMessage("Would you like to?")
-               .setPositiveButton("Upload a photo") { dialog, which ->
+               .setPositiveButton("Upload a photo") { _, _ ->
+                   // If the user choose to upload the photo we open Google Photo
                    if (auth.currentUser != null) {
                        selectImage()
                    }
                }
-               .setNegativeButton("Take a photo") { dialog, which ->
+               .setNegativeButton("Take a photo") { _, _ ->
+                   // If the user choose to upload the photo we open the device camera
                    dispatchTakePictureIntent()
                }
                .show()
         }
     }
+
+
+
+
 
 
 
@@ -211,6 +242,37 @@ class CreateEventFragment : Fragment() {
         }
     }
 
+
+
+
+
+
+
+
+    private fun dispatchTakePictureIntent() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_IMAGE_CAPTURE
+            )
+        } else {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+
+
+
+
+
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
@@ -227,6 +289,10 @@ class CreateEventFragment : Fragment() {
 
 
 
+    /**
+     * This method is used to upload the image, once selected or taken, to the Firebase Storage.
+     * Notes: It gets called in createEventListener().
+     */
     private fun uploadImageToFirebase(fileUri: Uri, photoUrl: String) {
         val userId = FirebaseAuth.getInstance().currentUser?.uid
         val storageReference = Firebase.storage(STORAGE_URL).reference.child("event")
@@ -262,22 +328,7 @@ class CreateEventFragment : Fragment() {
 
 
 
-    private fun dispatchTakePictureIntent() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.CAMERA),
-                REQUEST_IMAGE_CAPTURE
-            )
-        } else {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        }
-    }
+
 
 
     override fun onRequestPermissionsResult(

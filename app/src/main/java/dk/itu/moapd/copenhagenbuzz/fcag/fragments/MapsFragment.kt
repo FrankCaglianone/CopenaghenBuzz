f43d278418b9ed5fd9195b84166be1002f0cc86f
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-26 18:05:51 +0200

    MapsFragment code cleaning

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/fragments/MapsFragment.kt
@@ -9,8 +9,6 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -67,14 +65,27 @@ class MapsFragment : Fragment() {
     }
 
 
+    // Initializing variables used for user location fetching
     private lateinit var receiver: BroadcastReceiver
     private var latitude: Double? = null
     private var longitude: Double? = null
 
 
+
+
+    // Called when the fragment is being created.
+    /**
+     * As soon as the fragment is created it checks if location permissions are enabled.
+     * If not it asks the user to enable them, this is done because upon fragment creation we start
+     * fetching the user location to than set up the CameraUpdate to focus on the user's current
+     * location calling the method startLocalizationService().
+     * Once the current location of the user is fetched we call initializeMap() to initialize
+     * Google map's Map.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Check if location permissions are enabled otherwise ask for them
         if (!checkPermission()) {
             requestUserPermissions()
         }
@@ -88,12 +99,13 @@ class MapsFragment : Fragment() {
                 Log.d(TAG, latitude.toString())
                 Log.d(TAG, longitude.toString())
 
-                // Once the location is received, initialize the map
+                // Once the location is received, initialize the map. Note: done for Camera Update
                 initializeMap()
             }
         }
         val filter = IntentFilter(LocationService.LOCATION_UPDATE_ACTION)
         requireActivity().registerReceiver(receiver, filter)
+
 
         // Start fetching the user location when fragment is created
         startLocalizationService()
@@ -101,6 +113,8 @@ class MapsFragment : Fragment() {
 
 
 
+
+    // Called to create the layout of the fragment.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View = FragmentMapsBinding.inflate(inflater, container, false).also {
@@ -111,6 +125,7 @@ class MapsFragment : Fragment() {
 
 
 
+    // Called after the view created by onCreateView() has been created and ensures that the view hierarchy is fully initialized.
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
@@ -118,7 +133,7 @@ class MapsFragment : Fragment() {
 
 
 
-
+    // Called when the view previously created by onCreateView() has been detached from the fragment.
     override fun onDestroyView() {
         super.onDestroyView()
 
@@ -135,10 +150,11 @@ class MapsFragment : Fragment() {
 
 
 
-
-
-
-
+    /**
+     * This function initializes Google's Map using the callback.
+     * The function gets called only after the Broadcast Receiver received the user location
+     * to set up the CameraUpdate to focus on the current user's location.
+     */
     private fun initializeMap() {
         // Check if latitude and longitude are available
         if (latitude != null && longitude != null) {
@@ -155,13 +171,22 @@ class MapsFragment : Fragment() {
 
 
 
-
+    /**
+     * This is the callback passed to getMapAsync in initializeMap().
+     * This callback:
+     *      - sets up the CameraUpdate to focus on the user's current position
+     *      - sets the padding of the map
+     *      - double checks if the location permissions are enabled
+     *      - add the markers for all the events in the database
+     *      - creates the dialogs with the information of each event and the possibility to get
+     *          directions to the location of such event in google maps
+     */
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
 
         if(latitude != null && longitude != null) {
-            val userPosn = LatLng(latitude!!, longitude!!)
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(userPosn))
+            val userPosition = LatLng(latitude!!, longitude!!)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(userPosition))
         }
 
         // Move the Google Maps UI buttons under the OS top bar.
@@ -174,30 +199,29 @@ class MapsFragment : Fragment() {
             requestUserPermissions()
         }
 
-
+        // Add the markers for all the events in the database
         addMarkers(googleMap)
 
+        // Set up the dialog to show event info and open location directions in google maps
         context?.let { openGoogleMaps(googleMap, it) }
     }
 
 
-
-
-
-
-
-
+    /**
+     * This function simply checks for location permissions and returns a boolean
+     *      - true: if both of them are granted
+     *      - false: if one of them or both are not grated
+     */
     private fun checkPermission() =
         ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
 
 
-
-
-
-
-
+    /**
+     * This function request the user to enable location permissions.
+     * It gets called in case location permissions were not previously enabled by the user
+     */
     private fun requestUserPermissions() {
         if (!checkPermission())
             ActivityCompat.requestPermissions(
@@ -214,6 +238,11 @@ class MapsFragment : Fragment() {
 
 
 
+    /**
+     * This function is called on fragment creation to start fetching the user location to than
+     * set up the CameraUpdate to focus on the user's current position.
+     * The process continues in background until the fragment gets destroyed.
+     */
     private fun startLocalizationService() {
         Intent(requireContext(), LocationService::class.java).apply {
             action = LocationService.ACTION_START
@@ -225,6 +254,11 @@ class MapsFragment : Fragment() {
 
 
 
+    /**
+     * This function is called on fragment destruction to stop fetching the current user position.
+     * This is done purposely to minimize battery usage during the app utilization if the user
+     * is not looking at the map
+     */
     private fun stopLocalizationService() {
         Intent(requireContext(), LocationService::class.java).apply {
             action = LocationService.ACTION_STOP
@@ -237,6 +271,18 @@ class MapsFragment : Fragment() {
 
 
 
+    /**
+     * This function fetches all the events created from the Firebase realtime database and sets up
+     * a marker for it on the map.
+     * It also saves all the useful information of that event to than display them in a dialog when
+     * the marker is clicked; such as:
+     *      - name
+     *      - location (Address)
+     *      - date
+     *      - type
+     *      - description
+     *
+     */
     private fun addMarkers(map: GoogleMap) {
         FirebaseAuth.getInstance().currentUser?.let { user ->
             val userEventsRef = Firebase.database(DATABASE_URL).reference.child("copenhagen_buzz")
@@ -280,6 +326,13 @@ class MapsFragment : Fragment() {
 
 
 
+    /**
+     * This function sets the click listeners for the markers in the map.
+     * It creates an AlertDialog displaying the Event name and all useful info about that event.
+     * It also asks if we would like to get the directions to that event in google maps and if
+     * clicking yes, it automatically opens goggle maps fetching the user location and calculating
+     * the shortest path to get to that event.
+     */
     private fun openGoogleMaps(map: GoogleMap, context: Context) {
         map.setOnMarkerClickListener { marker ->
 

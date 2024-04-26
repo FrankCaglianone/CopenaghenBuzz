54b09aa2f7a6361187b7e9d7fee441caa9233aff
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-26 20:40:59 +0200

    Code cleaning in DataViewModel

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/models/DataViewModel.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/models/DataViewModel.kt
@@ -1,34 +1,10 @@
 package dk.itu.moapd.copenhagenbuzz.fcag.models
 
-import androidx.camera.core.CameraSelector
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 
 
 class DataViewModel : ViewModel() {
-
-    /**
-     * The current selected camera.
-     */
-    private var _selector = MutableLiveData<CameraSelector>()
-
-    /**
-     * A `LiveData` which publicly exposes any update in the camera selector.
-     */
-    val selector: LiveData<CameraSelector>
-        get() = _selector
-
-    /**
-     * This method will be executed when the user interacts with the camera selector component. It
-     * sets the selector into the LiveData instance.
-     *
-     * @param selector A set of requirements and priorities used to select a camera.
-     */
-    fun onCameraSelectorChanged(selector: CameraSelector) {
-        this._selector.value = selector
-    }
-
 
 
 

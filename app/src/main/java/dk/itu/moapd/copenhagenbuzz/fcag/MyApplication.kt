5182dd73f02fe01377322905996f66ee583383a8
Francesco Caglianone <francescocaglianone@gmail.com>
2024-04-26 20:45:53 +0200

    MyApplication docstring

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MyApplication.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/fcag/MyApplication.kt
@@ -11,12 +11,15 @@ import com.google.android.material.color.DynamicColors
 
 class MyApplication : Application(), OnMapsSdkInitializedCallback {
 
+    // A set of private constants used in this class.
     companion object {
         var TAG = "MyApplication"
     }
 
+
     override fun onCreate() {
         super.onCreate()
+
         // Apply dynamic colors to activities if available.
         DynamicColors.applyToActivitiesIfAvailable(this)
 
@@ -24,14 +27,24 @@ class MyApplication : Application(), OnMapsSdkInitializedCallback {
         MapsInitializer.initialize(this, MapsInitializer.Renderer.LATEST, this);
 
 
-        val channel = NotificationChannel(
-            "location", "location", NotificationManager.IMPORTANCE_LOW
-        )
+        // Creates a notification channel for location updates.
+        val channel = NotificationChannel("location", "location", NotificationManager.IMPORTANCE_LOW)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
     }
 
 
+
+
+    /**
+     * Callback method triggered when the Google Maps SDK initialization process completes.
+     * This method logs which version of the Maps renderer is used—either the latest or the legacy.
+     * Implementing this method allows the application to respond accordingly based on the renderer
+     * version that is initialized.
+     *
+     * @param renderer The renderer version of the Google Maps SDK that was initialized.
+     *                 This can either be `MapsInitializer.Renderer.LATEST` or `MapsInitializer.Renderer.LEGACY`.
+     */
     override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
         when (renderer) {
             MapsInitializer.Renderer.LATEST -> {
